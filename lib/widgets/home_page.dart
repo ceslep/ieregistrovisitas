6adb775dd611e5b89e77074d1887b05056a39ad5
ceslep <ceslep@gmail.com>
2024-03-14 12:36:01 -0500

    formulariolisto

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -9,15 +9,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _getDate() {
-    fecha = DateFormat('yyyy-mm-dd').format(DateTime.now());
+    fecha = DateFormat('yyyy-MM-dd').format(DateTime.now());
     hora = DateFormat('hh:mm:ss').format(DateTime.now());
     setState(() {});
   }
@@ -141,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        tooltip: 'Increment',
+        tooltip: 'Guardar Registro de visitas',
         child: const Icon(Icons.save),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
